a3b2af6de72922cb6d2a8e6bbfe4719c37fd22e7
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-08-17 21:46:52 +0300

    About responsive

--- client/src/pages/Home/components/About/AboutCard/AboutCard.jsx
+++ client/src/pages/Home/components/About/AboutCard/AboutCard.jsx
@@ -3,27 +3,30 @@ import { Box, Card, CardHeader, Grid, styled, Typography } from "@mui/material"
 const AboutCard = ({ cardContent }) => {
     const StyledCard = styled(Card)(({ theme }) => ({
         textAlign: "center",
-        padding: `${theme.spacing(4)} ${theme.spacing(4)}`,
         height: "100%",
         transition: ".2s",
         background: theme.palette.background.secondary,
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-
+        
         "&:hover": {
             transform: "scale(1.05)"
-        }
+        },
+
+        padding: `${theme.spacing(4)}`,
+        [theme.breakpoints.down("md")]: { padding: `${theme.spacing(2)}` },
+        [theme.breakpoints.down("sm")]: { padding: `${theme.spacing(4)}` }
     }))
 
 
 
     return (
-        <Grid size={4}>
+        <Grid size={{ xs: 12, sm: 6, lg:4 }}>
             <StyledCard variant="outlined">
                 {
                     cardContent.icon &&
-                    <Box sx={{ display: "flex", width: "30%" }} mb={3}>
+                    <Box sx={{ display: "flex", width: { xs: "30%", sm: "40%", md: "30%" } }} mb={3}>
                         <img className="image" src={cardContent?.icon} alt={cardContent?.title} />
                     </Box>
                 }
