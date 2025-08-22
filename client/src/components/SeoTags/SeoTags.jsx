8d60614d2c4dcb7977955995b4a642c9025b878f
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-08-23 00:39:13 +0300

    Add social media tags

--- client/src/components/SeoTags/SeoTags.jsx
+++ client/src/components/SeoTags/SeoTags.jsx
@@ -1,7 +1,9 @@
 import { useTranslation } from "react-i18next"
+import { useParams } from "react-router-dom"
 
 const SeoTags = () => {
     const { t } = useTranslation()
+    const { lng } = useParams()
     const baseUrl = "http://localhost:5173"
 
 
@@ -11,9 +13,20 @@ const SeoTags = () => {
             <title>{t("seo.title")}</title>
             <meta name="description" content={t("seo.description")} />
 
-            <link rel="alternate" href={`${baseUrl}/en`} hreflang="en" />
-            <link rel="alternate" href={`${baseUrl}/bg`} hreflang="bg" />
-            <link rel="alternate" href={`${baseUrl}/en`} hreflang="x-default" />
+            <link rel="alternate" href={`${baseUrl}/en`} hrefLang="en" />
+            <link rel="alternate" href={`${baseUrl}/bg`} hrefLang="bg" />
+            <link rel="alternate" href={`${baseUrl}/en`} hrefLang="x-default" />
+
+            < meta property="og:title" content={t("seo.title")} />
+            <meta property="og:description" content={t("seo.description")} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${baseUrl}/${lng}`} />
+            <meta property="og:image" content={`${baseUrl}/hero-desktop.webp`} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={t("seo.title")} />
+            <meta name="twitter:description" content={t("seo.description")} />
+            <meta name="twitter:image" content={`${baseUrl}/hero-desktop.webp`} />
         </>
     )
 }
