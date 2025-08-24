da59efc7b2c5f08ebf228714d9d5d79a8027f0a1
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-08-25 01:33:38 +0300

    Animate hero images

--- client/src/pages/Home/components/Hero/components/ImageComposition/ImageComposition.jsx
+++ client/src/pages/Home/components/Hero/components/ImageComposition/ImageComposition.jsx
@@ -1,13 +1,46 @@
-import { Box } from "@mui/material"
+import { Box, duration } from "@mui/material"
 import desktop from "../../../../../../img/hero-desktop.webp"
 import phone from "../../../../../../img/hero-phone.webp"
 import tablet from "../../../../../../img/hero-tablet.webp"
 import charm from "../../../../../../img/hero-charm.webp"
 import ImageContainer from "../../../../../../components/ImageContainer/ImageContainer"
+import { motion } from "motion/react"
 
 const ImageComposition = () => {
+    const containerVariants = {
+        initial: {
+            scale: 0
+        },
+        animate: {
+            scale: 1,
+            transition: {
+                duration: 0.5,
+                when: "beforeChildren",
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const childrenVariants = {
+        initial: {
+            scale: 0
+        },
+        animate: {
+            scale: 1,
+            transition: {
+                duration: 0.3,
+                type: "spring"
+            }
+        }
+    }
+
+
+
     return (
-        <Box sx={{ position: "relative" }} px={{ xs: 12, sm: 16, md: 4, lg: 12 }}>
+        <Box
+            sx={{ position: "relative" }}
+            px={{ xs: 12, sm: 16, md: 4, lg: 12 }}
+        >
 
             <ImageContainer
                 src={desktop}
@@ -15,7 +48,12 @@ const ImageComposition = () => {
                 props={{
                     sx: {
                         position: "relative"
-                    }
+                    },
+                    component: motion.div,
+                    variants: containerVariants,
+                    initial: "initial",
+                    whileInView: "animate",
+                    viewport: { once: true }
                 }}
             >
                 <ImageContainer
@@ -30,7 +68,9 @@ const ImageComposition = () => {
                             left: "-15%",
                             background: "white",
                             borderRadius: "20px",
-                        }
+                        },
+                        component: motion.div,
+                        variants: childrenVariants
                     }}
                 />
 
@@ -46,24 +86,10 @@ const ImageComposition = () => {
                             right: "-25%",
                             background: "white",
                             borderRadius: "20px",
-                        }
+                        },
+                        component: motion.div,
+                        variants: childrenVariants
                     }}
-                />
-
-                <ImageContainer 
-                    src={charm}
-                    alt="Charm"
-                    props={{
-                        sx: {
-                            position: "absolute",
-                            zIndex: 1,
-                            width: "25%",
-                            top: "-12%",
-                            right: 0,
-                            borderRadius: "20px"
-                        }
-                    }}
-                    imgClass="charm"
                 />
 
                 <ImageContainer 
@@ -77,9 +103,29 @@ const ImageComposition = () => {
                             top: "10%",
                             right: "-20%",
                             borderRadius: "20px"
-                        }
+                        },
+                        component: motion.div,
+                        variants: childrenVariants
                     }}
                     imgClass="charm reverse"
+                />
+
+                <ImageContainer 
+                    src={charm}
+                    alt="Charm"
+                    props={{
+                        sx: {
+                            position: "absolute",
+                            zIndex: 1,
+                            width: "25%",
+                            top: "-12%",
+                            right: 0,
+                            borderRadius: "20px"
+                        },
+                        component: motion.div,
+                        variants: childrenVariants
+                    }}
+                    imgClass="charm"
                 />
             </ImageContainer>
 
