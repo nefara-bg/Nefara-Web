786c66e2c4572762ef118f8abe03c63cade43639
MartiiPartii <velchev061@gmail.com>
2025-11-08 15:05:56 +0200

    Create the root layout

---
+++ client/src/app/layout.jsx
@@ -0,0 +1,20 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+        <head>
+            <link rel="icon" type="image/png" href="/tab-logo.png" media="(prefers-color-scheme: light)" />
+            <link rel="icon" type="image/png" href="/tab-logo-dark.png" media="(prefers-color-scheme: dark)" />
+            <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
+        </head>
+        <body>
+            <div id="root">
+                {children}
+            </div>
+        </body>
+    </html>
+  )
+}
