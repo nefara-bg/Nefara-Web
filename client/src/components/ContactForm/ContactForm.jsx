6e8a8f52d90264620b46e778a729fa80712cd1c8
MartiiPartii <velchev061@gmail.com>
2025-11-12 09:47:57 +0200

    Fix again

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -7,7 +7,8 @@ import { useRef, useState } from "react"
 import axios from "axios"
 import InfoFeature from "@/components/InfoFeature/InfoFeature"
 import { motion } from "motion/react"
-dynamic(() => import("react-emoji-render").then(mod => mod.Twemoji), { ssr: false })
+import dynamic from "next/dynamic"
+const Twemoji = dynamic(() => import("react-emoji-render").then(mod => mod.Twemoji), { ssr: false })
 
 const ContactForm = ({ phone = "", phoneLabel = "" }) => {
     const t = useTranslations()
