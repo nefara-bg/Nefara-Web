ab328ef9e26806a0809c58f6e19b582e89856301
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-11-27 16:39:32 +0200

    Setup secure configuration

--- client/src/utils/email/transporter.js
+++ client/src/utils/email/transporter.js
@@ -1,14 +1,22 @@
 import nodemailer from "nodemailer";
 
 export const createPrivateEmailTransport = async () => {
+    const port = Number(process.env.SMTP_PORT);
+    const secure = port === 465
+    
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        // secure: process.env.SMTP_SECURE === "true", // true = 465
+        port: port,
+        secure: secure,
+        requireTLS: !secure,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
-        }
+        },
+        
+        connectionTimeout: 5000,
+        socketTimeout: 5000,
+        greetingTimeout: 5000,
     });
 
     return transporter
