f9789a571808b55dfc3aca340f0d3202a14f7851
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-11-27 11:19:59 +0200

    Send emails

--- client/src/services/contact.js
+++ client/src/services/contact.js
@@ -1,20 +1,27 @@
-export const sendEmail = async (body) => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/contact`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body)
-    })
+import { Resend } from "resend"
 
-    if(res.ok) {
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export const sendEmail = async (body) => {
+    try {
+        const { error } = await resend.emails.send({
+            from: "Contact Us from Nefara! <contacts@nefara.org>",
+            to: [process.env.CONTACT_EMAIL],
+            subject: body.subject,
+            text: body.message,
+            replyTo: body.email
+        })
+
+        if(error) return {
+            success: false,
+            error: "Something went wrong. Please try again."
+        }
+        else return {
+            success: true
+        }
+    } catch(e) {
         return {
             success: true
         }
-    }
-
-    return {
-        success: false,
-        error: "Something went wrong. Please try again."
     }
 }
